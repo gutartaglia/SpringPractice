56e30b8dfaae1637316b006e1038877017fd6deb
gutartaglia <spotguga@gmail.com>
2026-04-21 20:27:11 -0300

    OrderStatus enum

--- src/main/java/com/example/SpringPractice/entities/Order.java
+++ src/main/java/com/example/SpringPractice/entities/Order.java
@@ -3,6 +3,7 @@ package com.example.SpringPractice.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.example.SpringPractice.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -25,15 +26,18 @@ public class Order implements Serializable{
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_Id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant moment,User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -43,6 +47,16 @@ public class Order implements Serializable{
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public Long getId() {
