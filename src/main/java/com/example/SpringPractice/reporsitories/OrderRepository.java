9091041d3d9ce30e3113d11e230744a19adf8a81
gutartaglia <spotguga@gmail.com>
2026-04-21 18:49:57 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/example/SpringPractice/reporsitories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.SpringPractice.reporsitories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.SpringPractice.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+    
+}  
