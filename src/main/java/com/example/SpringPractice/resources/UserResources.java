e27e5139152956a2733e300c93cecbb320286c2a
gutartaglia <spotguga@gmail.com>
2026-06-04 00:59:13 -0300

    User insert

--- src/main/java/com/example/SpringPractice/resources/UserResources.java
+++ src/main/java/com/example/SpringPractice/resources/UserResources.java
@@ -1,15 +1,21 @@
 package com.example.SpringPractice.resources;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.example.SpringPractice.entities.User;
 import com.example.SpringPractice.services.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -34,5 +40,11 @@ public class UserResources {
         return ResponseEntity.ok().body(obj);
     } 
     
-
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+    
 }
