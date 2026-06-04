83c73373cdb914dcc71c4128b86c7deb3bfff8e4
gutartaglia <spotguga@gmail.com>
2026-06-04 14:23:03 -0300

    Exception handling - update

--- src/main/java/com/example/SpringPractice/services/UserService.java
+++ src/main/java/com/example/SpringPractice/services/UserService.java
@@ -12,6 +12,8 @@ import com.example.SpringPractice.reporsitories.UserRepository;
 import com.example.SpringPractice.services.exceptions.DatabaseException;
 import com.example.SpringPractice.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -44,9 +46,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
