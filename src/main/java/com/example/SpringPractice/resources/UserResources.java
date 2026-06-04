31d610565b2f9a4ac06ed434e989786ba32be021
gutartaglia <spotguga@gmail.com>
2026-06-04 01:04:59 -0300

    Deleção de User

--- src/main/java/com/example/SpringPractice/resources/UserResources.java
+++ src/main/java/com/example/SpringPractice/resources/UserResources.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.SpringPractice.entities.User;
 import com.example.SpringPractice.services.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,12 @@ public class UserResources {
         obj = userService.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
     
 }
